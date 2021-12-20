ef1298174aa66308290fa8d1ced04475738b442e
Andrés <sandresf@uninorte.edu.co>
2021-12-20 18:56:26 -0500

    Cambios database

--- src/database.js
+++ src/database.js
@@ -11,41 +11,6 @@ mongoose
     .connect(mongo_uri)
     .then(() => console.log('Conectado a MongoDB Atlas'))
     .catch((error) => console.error(error));
+   
 
-app.post('/register', async (req,res) => {
-    const {username, password} = req.body;
-    const user = await new User({username,password});
-
-    user.save(err => {
-        if(err){
-            res.status(500).send('Error al registrarse')
-            console.log(err)
-        } else {
-            res.status(200).send('Usuario registrado exitosamente!')
-        }
-    })
-});
-
-app.post('/authenticate', (req,res) => {
-    const {username, password} = req.body;
-    user.findOne({username}, (err, user) => {
-        if(err){
-            res.status(500).send('Error al login')
-        } else if (!user){
-            res.status(500).send('Usuario no existe!')
-        } else {
-            user.isCorrectPassword(password, (err,result) => {
-                if(err){
-                    res.status(500).send('Error al login')
-                } else if(result){
-                    res.status(200).send('Usuario ingresado exitosamente!')
-                } else {
-                    res.status(500).send('Usuario no ingreso.')
-                }
-            })
-        } 
-    })
-});
-    
-
-    module.exports= mongoose;
+module.exports= mongoose;
