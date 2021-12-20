dad0c3dd19b0eadacce35cffcf1a7c185ef662d2
Andrés <sandresf@uninorte.edu.co>
2021-12-20 18:23:04 -0500

    Cambios db

--- src/database.js
+++ src/database.js
@@ -1,9 +1,51 @@
 //ingresar la info de la base de datos
 const mongoose = require('mongoose');
+const express = require('express');
+
+const app = express();
+
+const mongo_uri = 'mongodb://localhost/choco-peajes';
 
 mongoose
-    .connect(process.env.MONGODB_URI)
+    // .connect(process.env.MONGODB_URI)
+    .connect(mongo_uri)
     .then(() => console.log('Conectado a MongoDB Atlas'))
     .catch((error) => console.error(error));
 
-module.exports= mongoose;
+app.post('/register', async (req,res) => {
+    const {username, password} = req.body;
+    const user = await new User({username,password});
+
+    user.save(err => {
+        if(err){
+            res.status(500).send('Error al registrarse')
+            console.log(err)
+        } else {
+            res.status(200).send('Usuario registrado exitosamente!')
+        }
+    })
+});
+
+app.post('/authenticate', (req,res) => {
+    const {username, password} = req.body;
+    user.findOne({username}, (err, user) => {
+        if(err){
+            res.status(500).send('Error al login')
+        } else if (!user){
+            res.status(500).send('Usuario no existe!')
+        } else {
+            user.isCorrectPassword(password, (err,result) => {
+                if(err){
+                    res.status(500).send('Error al login')
+                } else if(result){
+                    res.status(200).send('Usuario ingresado exitosamente!')
+                } else {
+                    res.status(500).send('Usuario no ingreso.')
+                }
+            })
+        } 
+    })
+});
+    
+
+    module.exports= mongoose;
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,17 +5,52 @@ const router = express.Router();
 
 // Create Users
 
-router.post('/users', (req,res)=>{
-    const user = userSchema(req.body);
-    user
-        .save()
-        .then((data)=> res.json(data))
-        .catch((error)=>console.error({message: error}))
+// router.post('/users', (req,res)=>{
+//     const user = userSchema(req.body);
+//     user
+//         .save()
+//         .then((data)=> res.json(data))
+//         .catch((error)=>console.error({message: error}))
+// });
+
+router.post('/register', async (req,res) => {
+    const {username, password} = req.body;
+    const user = await new User({username,password});
+
+    user.save(err => {
+        if(err){
+            res.status(500).send('Error al registrarse')
+            console.log(err)
+        } else {
+            res.status(200).send('Usuario registrado exitosamente!')
+        }
+    })
+});
+
+router.post('/authenticate', (req,res) => {
+    const {username, password} = req.body;
+    user.findOne({username}, (err, user) => {
+        if(err){
+            res.status(500).send('Error al login')
+        } else if (!user){
+            res.status(500).send('Usuario no existe!')
+        } else {
+            user.isCorrectPassword(password, (err,result) => {
+                if(err){
+                    res.status(500).send('Error al login')
+                } else if(result){
+                    res.status(200).send('Usuario ingresado exitosamente!')
+                } else {
+                    res.status(500).send('Usuario no ingreso.')
+                }
+            })
+        } 
+    })
 });
 
 // Get all users
 
-router.post('/users', (req,res)=>{
+router.get('/register', (req,res)=>{
     const user = userSchema(req.body);
     user
         .find()
